ec7449755fb120171f169db8ae63058ba02a2810
David Rivera <jherax@gmail.com>
2024-07-17 19:51:30 -0500

    chore: Improved debounce() function

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,20 +1,25 @@
-type AnyFunction = (...args: any[]) => any;
-
 /**
  * Creates a debounced version of the `callback` that controls
  * how many times we allow a function to be executed over time.
  *
+ * @see https://cutt.ly/debounce-throttle
+ *
  * @param callback The function to defer the execution
  * @param delay Milliseconds to delay the execution
  */
-export default function debounce<TFunc extends AnyFunction>(callback: TFunc, delay: number) {
-  let timer: NodeJS.Timeout;
-  
-  return function debouncedCallback(...args: Parameters<typeof callback>) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback(...args);
-      clearTimeout(timer);
-    }, delay);
+export default function debounce<Args extends unknown[], R>(
+  callback: (...args: Args) => R,
+  delay: number,
+) {
+  let taskId: number | NodeJS.Timeout | null;
+
+  return function debouncedCallback(...args: Args) {
+    if (taskId == null) {
+      taskId = setTimeout(() => {
+        callback(...args);
+        clearTimeout(taskId as number);
+        taskId = null;
+      }, delay);
+    }
   };
 }
